fb31976a24b67a2f8ba600a344a708d95002adf9
asomos <asomos@shutterstock.com>
2020-05-30 23:44:05 -0400

    root commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1>Anthony Somos</h1>
+<p>
+  Hi there. Thanks for visiting my .dev site. I set up this website for when I
+  have something to share, and need a place to share it.
+</p>
